af38bd8171f62c44c4186daa83213a709220403b
Aditya Goyal <43295337+adityagoyal222@users.noreply.github.com>
2020-10-05 15:20:16 +0545

    created views for comments and added login template

--- cloneblog/urls.py
+++ cloneblog/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'', include('blogs.urls')),
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name="login"),
+    url(r'^accounts/logout/$', views.logout, name="login", kwargs={'next_page': '/'}),
 ]
